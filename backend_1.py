d035c23eb1b0d0f7d2b7dbec256676cee941853c
ziqiqielsie <118112530+ziqiqielsie@users.noreply.github.com>
2023-02-01 20:59:10 +0800

    Add files via upload

--- backend_1.py
+++ backend_1.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-personal_normal_waiting = [2,4,5]
+personal_normal_waiting = [2,4]
 personal_priority_waiting = [3]
-business_normal_waiting = [1,2,3]
+business_normal_waiting = []
 personal_skipped = []
 business_skipped = []
-current_queue_no = 1
+current_queue_no = 0
 
 def get_next_personal_customer():
     """
@@ -53,9 +53,10 @@ def skip_personal_customer():
     else:
         return 'Cannot skip, no customer in the queue'
 
+
 def skip_business_customer():
     global current_queue_no
-    global personal_skipped
+    global business_skipped
 
     if current_queue_no != 'No customer in the queue':
         business_skipped.append(current_queue_no) # Append the skipped queue number to the business_skipped list
@@ -65,33 +66,52 @@ def skip_business_customer():
         return 'Cannot skip, no customer in the queue'
 
 
-@app.route('/counter/jp_p_2', methods=['GET','POST'])
+@app.route('/counter/counter_id', methods=['GET','POST'])
 def next():
 
     button = request.form.get("button1") if request.form.get("button1") else request.form.get("button2")
     if request.method == 'POST' and button == "next":
         current_queue_no = get_next_personal_customer()
-        return render_template('counter_page_jp_p_2.html', q=current_queue_no)
+        return render_template('counter_page_1.html', q=current_queue_no)
     elif request.method == "POST" and button == "skip":
         current_queue_no = skip_personal_customer()
-        return render_template('counter_page_jp_p_2.html', q=current_queue_no)
-    return render_template('counter_page_jp_p_2.html', q=None)
+        return render_template('counter_page_1.html', q=current_queue_no)
+
+    return render_template('counter_page_1.html', q=None)
 
 
-@app.route('/counter/jp_b_1', methods=['GET','POST'])
-def next2():
+def counter_name_parser(counter_id):
+    branch_dict = {'jp':'Jurong Point',
+                   'je': 'Jurong East',
+                   'kl': 'Kuala Lumpur',
+                   'hg': 'Hou Gang'}
+    business_dict = {'p':'personal',
+                     'b':'business'}
 
+    branch_id = counter_id[:2]
+    branch = branch_dict[branch_id]
+    type_id = counter_id[3]
+    type_of_business = business_dict[type_id]
+    counterId = counter_id[5:]
+    name = f"{branch} {type_of_business} {counterId}"
+
+    return name
+
+@app.route('/counter/<counter_id>', methods=['GET','POST'])
+def show(counter_id):
+    name = counter_name_parser(counter_id)
     button = request.form.get("button1") if request.form.get("button1") else request.form.get("button2")
+
     if request.method == 'POST' and button == "next":
-        current_queue_no = get_next_business_customer()
-        return render_template('counter_page_jp_b_1.html', q=current_queue_no)
+        current_queue_no = get_next_personal_customer()
+        return render_template('counter_page_1.html', counterName=name, q=current_queue_no)
     elif request.method == "POST" and button == "skip":
-        current_queue_no = skip_business_customer()
-        return render_template('counter_page_jp_b_1.html', q=current_queue_no)
-    return render_template('counter_page_jp_b_1.html', q=None)
+        current_queue_no = skip_personal_customer()
+        return render_template('counter_page_1.html', counterName=name, q=current_queue_no)
+
+    return render_template('counter_page_1.html', counterName=name,  q=None)
 
 
 if __name__ == '__main__':
-    app.run(host = "localhost",debug = True,port=8080)
-
+    app.run(debug = True,port=8080)
 
