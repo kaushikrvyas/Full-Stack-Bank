b4b465b5a48ebbbe6c03e8d9b8828548aa79af43
ziqiqielsie <118112530+ziqiqielsie@users.noreply.github.com>
2023-02-03 19:19:54 +0800

    Add files via upload

--- backend_1.py
+++ backend_1.py
@@ -200,7 +200,9 @@ def get_q_mobile(user):
 
         if type_of_business is not None and priority is not None and branch is not None:
             current_assigned_queue_no = assign_queue_no_to_queue(branch, type_of_business, priority)
-            return render_template('queue_generated.html',user=user)
+            type_of_business = business_dict[type_of_business]
+            branch = branch_dict[branch]
+            return render_template('queue_generated.html',user=user, q_number=current_assigned_queue_no, type_of_business=type_of_business, branch=branch)
         else:  #if user doesn't select all value, prompt user to input all required information
             return render_template('queue_gen_fail.html',user=user)
     return render_template('mobile_queue_gen.html', branch_dict=branch_dict, business_dict=business_dict, priority_dict=priority_dict)
