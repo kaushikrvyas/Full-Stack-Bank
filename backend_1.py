15d0fde8d5abccf3f0b210a1dcc29b919768dfdb
TonyHuang615 <95336795+TonyHuang615@users.noreply.github.com>
2023-02-11 11:10:23 +0800

    Update backend_1.py

--- backend_1.py
+++ backend_1.py
@@ -21,7 +21,7 @@ def SendEmail(email_address,q_number, type_of_business,
     currenttime=now.strftime('%Y-%m-%d %H:%M:%S')
     msg = Message(
 				'Hello Please Check Your Queue Number',
-				sender ='tony615huang@gmail.com',
+				sender ='customerservice@ABCBank.sg',
 				recipients = ['{email_address}'.format(email_address=email_address)]
 			)
     msg.body = 'Dear user, \n\nThank you for choosing ABC Bank. \nYour Queue Number is {q_number} for {type_of_business} in {branch_name}. \nThere is {waiting_numbers} person(s) in the queue in front of you. \nThe estimated waiting time is {estimated_time}. \nThank you for choosing us, and we look forward to assisting you soon.\n\nBest wishes,\nABC Bank {branch_name} branch\n\n{currenttime}'.format(q_number=q_number, 
